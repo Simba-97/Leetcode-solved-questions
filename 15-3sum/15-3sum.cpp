2147139bd207d0d144c067ecbfcd7ad66ceecbb4
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-02-27 00:56:35 +0530

    Time: 188 ms (31.66%), Space: 23.6 MB (29.33%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -7,10 +7,13 @@ public:
         
         if(nums.size() < 3) return res;
         
+        //moving for a
         for(int i = 0; i < nums.size()-2; i++){
+                         // ignoring duplicate triplets
             if(i == 0 || (i >0 && nums[i] != nums[i-1])){
                 int low = i+1; int high = nums.size()-1;
                 int sum = 0 - nums[i];
+                //run till lo doesn't crosses high and vice versa
                 while(low < high){
                     if(nums[low] + nums[high] == sum){
                         vector<int> temp;
@@ -19,13 +22,15 @@ public:
                         temp.push_back(nums[high]);
                         res.push_back(temp);
                         
+                        //ignoring duplicate elements for lo
                         while(low < high  && nums[low] == nums[low+1]) low++;
+                        //ignoring duplicate elements for high
                         while(low < high  && nums[high] == nums[high-1]) high--;
                         
                         low++; high--;
-                    }
+                    }                                   //moving lo to increase the value of a
                     else if(nums[low]+nums[high] < sum) low++;
-                    else high--;
+                    else high--; // otherwise
                 }
             }
         }
