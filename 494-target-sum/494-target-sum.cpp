f31ea222288fb8f2e904e7011f0c465d44293623
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-03-15 22:57:38 +0530

    Time: 18 ms (64.90%), Space: 9.1 MB (78.12%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int i, s = 0;
+        s = accumulate(nums.begin(), nums.end(), 0);
+        
+        
+        target = abs(target);
+        
+        int  new_target = (s+target);
+        new_target = new_target/2;
+            
+        if((s+target) % 2 == 1 || s < new_target) return 0;
+     
+        //new_target = abs(new_target);
+        int dp[n+1][new_target+1];
+       
+        for(i = 0; i < new_target + 1; i++) dp[0][i]=0;
+        
+        for(i = 0; i < n+1; i++) dp[i][0]=1;
+ 
+
+        for(i = 1; i <= n; i++) {
+            for(int j = 0; j <= new_target; j++) {
+                 dp[i][j] = dp[i-1][j];
+                 if(nums[i-1] <= j)
+                    dp[i][j] = dp[i][j] + dp[i-1][j - nums[i-1]];
+                
+                //cout<<i<<" "<<j<<" "<<dp[i][j]<<endl;
+                   }
+        }  
+        
+        return dp[n][new_target];    
+    }
+};
