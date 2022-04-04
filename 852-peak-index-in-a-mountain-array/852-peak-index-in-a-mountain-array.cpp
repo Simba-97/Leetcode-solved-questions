b62334cd8c2a196808ffc1dd908414ee85b32816
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-04-05 01:10:15 +0530

    Time: 19 ms (23.47%), Space: 11.6 MB (5.11%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0; int right = arr.size() - 1;
         while(left < right) {
             int mid = left + (right - left)/2;
-            if(arr[mid] <= arr[mid+1]){
+            if(arr[mid] < arr[mid+1]){
                 left = mid + 1;
             } else {
                 right = mid;
