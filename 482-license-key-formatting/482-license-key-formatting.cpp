d011b0283e660681047bc39d2bb89edc31673379
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-05-04 00:18:36 +0530

    Time: 20 ms (15.89%), Space: 8.3 MB (43.87%) - LeetHub

---
+++ 482-license-key-formatting/482-license-key-formatting.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string ans;
+        int count = 0;
+
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] != '-') {
+                ans.push_back(toupper(s[i]));
+                count++;
+                if (count % k == 0) {
+                    ans.push_back('-');
+                }
+            }
+        }
+
+        if (ans.size() > 0 && ans.back() == '-') {
+            ans.pop_back();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
