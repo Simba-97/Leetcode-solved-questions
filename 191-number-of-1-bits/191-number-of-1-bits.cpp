f3943f237e9a1548b3f4398b92e45db695c5c76c
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-05-26 23:30:20 +0530

    Time: 0 ms (100.00%), Space: 6 MB (48.74%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -3,7 +3,13 @@ public:
     int hammingWeight(uint32_t n) {
         // string s = to_string(n);
         // cout << s << endl;
-        bitset<64> b1(n);
-        return b1.count();
+        // bitset<64> b1(n);
+        // return b1.count();
+        int count = 0;
+        while(n > 0) {
+            count += (n & 1);
+            n=n>>1;
+        }
+        return count;
     }
 };
