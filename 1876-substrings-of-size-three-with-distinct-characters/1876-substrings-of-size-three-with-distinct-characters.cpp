ca56493b49e148696502c96fd0746204f23bf76f
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-09-25 20:14:46 +0530

    Time: 19 ms (5.51%), Space: 8.1 MB (8.91%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
-    int countGoodSubstrings(string s) {
-        if(s.size() <= 1) return 0;
-        int count = 0;
-        for(int i = 0; i < s.size()-2; i++) {
-            if((s[i] != s[i+1]) and (s[i] != s[i+2]) && (s[i+1] != s[i+2])) {
-                count++;
+    int countGoodSubstrings(string se) {
+        // if(s.size() <= 1) return 0;
+        // int count = 0;
+        // for(int i = 0; i < s.size()-2; i++) {
+        //     if((s[i] != s[i+1]) and (s[i] != s[i+2]) && (s[i+1] != s[i+2])) {
+        //         count++;
+        //     }
+        // }
+        // return count;
+        set<int>s;
+        if(se.length()<3)return 0;
+        int maxi = 0;
+        int i;
+        for(i=0;i<se.length()-2;i++){
+            for(int j=i;j<(i+3);j++){
+                s.insert(se[j]);
             }
+            if(s.size()==3)maxi++;
+            s.clear();
         }
-        return count;
+            // for(int j=i;j<(i+3);j++){
+            //     s.insert(se[j]);
+            // }
+            // if(s.size()==3)maxi++;
+        return maxi;
     }
 };
