7d8ea8ac10e0e0ebcc2066cb0f0f57d775b150e9
Hritik Kumar Singh <singhhritik978@gmail.com>
2022-10-15 14:42:01 +0530

    Added solution - LeetHub

--- Find position of set bit - GFG/find-position-of-set-bit.cpp
+++ Find position of set bit - GFG/find-position-of-set-bit.cpp
@@ -5,25 +5,13 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    bool isPowerOfTwo(int n) {
-        long long a = n;
-        if(a == 0) return false;
-        if(a == 1) return true;
-        
-        if((a & (a-1)) == 0) {
-            return true;
-        }
-        return false;
-    }
     int findPosition(int N) {
         // code here
-        bool twoPower = isPowerOfTwo(N);
-        
-        if(twoPower) {
+        if(N == 0) return -1;
+        if((N & (N-1)) == 0) {
             return log2(N) + 1;
-        } else {
-            return -1;
         }
+        return -1;
     }
 };
 
