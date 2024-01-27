5d0ab6bec52f49e9bc8b05c5d22e84aa1de5edc2
Hritik Kumar Singh <singhhritik978@gmail.com>
2024-01-27 15:54:16 +0530

    Time: 5 ms (52.69%), Space: 9.1 MB (5.88%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,19 +3,21 @@ public:
     bool isPalindrome(string s) {
         int n = size(s);
         
-        if(n == 1) return true; // if string size 1 then simple return true if symbol is present no need worry about we don't need remove question ask return 
-        //true or false
+        if(n == 1) return true;
         
-        string str = "";
-        for(int i=0; i<n; i++) if(isalnum(s[i])) str +=s[i]; // only put number and char not any symbol
+        string newString;
+        for(int i = 0; i < n; i++){
+            if(iswalnum(s[i])){
+                newString += s[i];
+            }
+        }
+               
+        transform(newString.begin(), newString.end(), newString.begin(), ::tolower); 
+               
+        int i=0,j=size(newString)-1;
         
-        // converting whole string into uppercase
-        transform(str.begin(), str.end(), str.begin(), ::toupper);
-        
-        int i=0,j=size(str)-1;
-        
-        while(i<j) {
-            if(str[i] != str[j]) return false; // checking where two char are not same then return false
+        while(i < j) {
+            if(newString[i] != newString[j]) return false;
             i++,j--;
         }
         return true;
