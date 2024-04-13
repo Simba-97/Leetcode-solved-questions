4c0e4212482930fe29af2ea8279cabb5cffb35bc
Hritik Kumar Singh <singhhritik978@gmail.com>
2024-04-14 01:50:45 +0530

    Time: 9 ms (59.52%), Space: 18 MB (19.86%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            if(mp.find(target-nums[i]) != mp.end()){
-                vector<int> ans = {i+1, mp[target-nums[i]] + 1};
-                sort(ans.begin(), ans.end());
+        //using map
+        // unordered_map<int, int> mp;
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(mp.find(target-nums[i]) != mp.end()){
+        //         vector<int> ans = {i+1, mp[target-nums[i]] + 1};
+        //         sort(ans.begin(), ans.end());
+        //         return ans;
+        //     }
+        //     mp[nums[i]] = i;
+        // }
+        // return {-1, -1};
+        
+        //using binary search
+        int left = 0, right = nums.size() - 1;
+        vector<int> ans;
+        while(left <= right){
+            int sum = nums[left] + nums[right];
+            
+            if(sum == target){
+                ans.push_back(left + 1);
+                ans.push_back(right + 1);
                 return ans;
+            } else if(sum > target){
+                right--;
+            } else {
+                left++;
             }
-            mp[nums[i]] = i;
         }
         return {-1, -1};
     }
