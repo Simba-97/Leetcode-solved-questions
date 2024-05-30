14048856c49a03a19ff3ed11ff34aee62a6992c0
Hritik Kumar Singh <singhhritik978@gmail.com>
2024-05-31 01:27:01 +0530

    Time: 30 ms (45.00%), Space: 28.2 MB (14.88%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
+    string generateNewWord(string& word){
+        vector<int> v(26, 0);
+
+        for(int i = 0; i < word.size(); i++){
+            v[word[i] - 'a']++;
+        }
+
+        string newWord = "";
+        for(int i = 0; i < 26; i++){
+            if(v[i] > 0) {
+                newWord += string(v[i], i + 'a');
+            }
+        }
+
+        return newWord;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
+        // vector<vector<string>> ans;
+        // map<string, vector<string>> mp;
 
-        unordered_map <string, vector<string>> mMap; 
+        // for(int i = 0; i < strs.size(); i++){
+        //     string temp = strs[i];
+        //     sort(temp.begin(), temp.end());
 
-        for(int i = 0; i < strs.size(); i++) {
-            string s = strs[i];
-
-            sort(s.begin(), s.end());
-            mMap[s].push_back(strs[i]);
-        }
-
-        for(auto itr = mMap.begin(); itr != mMap.end(); itr++) {
-            result.push_back(itr->second);
-        }
-
+        //     mp[temp].push_back(strs[i]);
+        // }
         
-        return result;
+        // for(auto a: mp){
+        //     ans.push_back(a.second);
+        // }
+        // return ans;
+
+        vector<vector<string>> ans;
+        map<string, vector<string>> mp;
+
+        for(int i = 0; i < strs.size(); i++){
+            string temp = strs[i];
+            string new_word = generateNewWord(temp);
+
+            mp[new_word].push_back(strs[i]);
+        }
+        
+        for(auto &a: mp){
+            ans.push_back(a.second);
+        }
+        return ans;
     }
 };
