5b5d7d7c04dea19dba2f12320c0ef06825df7515
Hritik Kumar Singh <singhhritik978@gmail.com>
2024-06-04 00:06:13 +0530

    Time: 0 ms (100.00%), Space: 9 MB (94.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-    int t[101];
-    int solve(vector<int>& nums, int i, int n){
-        if(i >= n){
-            return 0;
-        }
+//     int t[101];
+//     int solve(vector<int>& nums, int i, int n){
+//         if(i >= n){
+//             return 0;
+//         }
         
-        if(t[i] != -1){
-            return t[i];
-        }
+//         if(t[i] != -1){
+//             return t[i];
+//         }
         
-        int steal = nums[i] + solve(nums, i+2, n);
-        int skip = solve(nums, i+1, n);
+//         int steal = nums[i] + solve(nums, i+2, n);
+//         int skip = solve(nums, i+1, n);
         
-        return t[i] = max(steal, skip);
-    }
+//         return t[i] = max(steal, skip);
+//     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(nums, 0, n);
+        if(n == 1)
+            return nums[0];
+        int t[n+1];
+        memset(t, 0, sizeof(t));
+        t[0] = 0;
+        t[1] = nums[0];
+        for(int i = 2; i <= n; i++){
+            int steal = nums[i-1] + t[i-2];
+            int skip = t[i-1];
+            
+            t[i] = max(steal, skip);
+        }
+        
+        return t[n];
     }
 };
