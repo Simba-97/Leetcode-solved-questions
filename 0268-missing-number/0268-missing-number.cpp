8a27df4903918c6c9a9fca35368dacd0ef707b55
Hritik Kumar Singh <singhhritik978@gmail.com>
2024-07-20 21:06:22 +0530

    Time: 8 ms (90.83%), Space: 20.7 MB (33.41%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0) return 0;
-        map<int, int> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
+        // int n = nums.size();
+        // if(n == 0) return 0;
+        // map<int, int> mp;
+        // for(int i=0;i<nums.size();i++){
+        //     mp[nums[i]]++;
+        // }
         
-        for(int i=0; i<=n; i++){
-            if(mp.find(i) == mp.end()){
-                return i;
-            }
+        // for(int i=0; i<=n; i++){
+        //     if(mp.find(i) == mp.end()){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        int n = nums.size();
+        int sum = (n * (n + 1))/2;
+
+        for(int i = 0; i < n; i++){
+            sum = sum - nums[i];
         }
-        return -1;
+
+        return sum;
     }
 };
